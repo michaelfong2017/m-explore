997022fa4ae1f6e4d028c95f810951c4dd99a11e
michaelfong2017 <michaelfong2017@gmail.com>
2023-01-13 04:40:03 +0800

    Pass correct traced robot name back to Traceback in order to prevent repeating the same image.

--- explore/src/explore.cpp
+++ explore/src/explore.cpp
@@ -392,6 +392,7 @@ void Explore::tracebackGoalAndImageUpdate(
     in_traceback_ = true;
     current_traceback_goal_ = msg->goal;
     current_traced_robot_image_ = msg->image;
+    current_traced_robot_ = msg->traced_robot;
     current_traced_robot_stamp_ = msg->stamp;
   }
 }
@@ -424,7 +425,7 @@ void Explore::reachedGoal(const actionlib::SimpleClientGoalState& status,
           images.traced_image = current_traced_robot_image_;
           // TODO also capture current image and send
           images.tracer_image = current_image_;
-          images.robot_name = getRobotName();
+          images.robot_name = current_traced_robot_;
           images.stamp = current_traced_robot_stamp_;
           traceback_image_and_image_publisher_.publish(images);
           makePlan();
